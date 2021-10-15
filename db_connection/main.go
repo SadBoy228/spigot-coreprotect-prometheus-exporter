7dd8ee50362203d6de77fa8f2c273bb386bbeafb
m15h4nya <i.am.ganter@gmail.com>
2021-10-15 12:05:10 +0300

    fixed db openning

--- db_connection/main.go
+++ db_connection/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/go-sql-driver/mysql"
+	"github.com/driver/mysql"
 	"github.com/k0tletka/spigot-coreprotect-prometheus-exporter/config"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -20,7 +20,7 @@ func InitializeConnection() *gorm.DB {
 		db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
 	} else {
-		dsn := fmt.Sprintf("%v:%v@tcp(127.0.0.1:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", cfg.DB.MySQLHostname, cfg.DB.MySQLPassword, cfg.DB.MySQLPort, cfg.DB.MySQLDatabaseName)
+		dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", cfg.DB.MySQLUsername, cfg.DB.MySQLPassword, cfg.DB.MySQLHostname, cfg.DB.MySQLPort, cfg.DB.MySQLDatabaseName)
 		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	}
 	if err != nil {
